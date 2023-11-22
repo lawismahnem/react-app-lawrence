f0653668e8fd5ec57d1fe1b51c26077886d6ad47
lawrence <lordlawrencesalazar@gmail.com>
2023-11-22 19:38:28 +0800

    first push

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {
+  title: string;
+};
+
+export default function Header({ title }: Props) {
+  return (
+    <div className="px-2 py-4 border-b">
+      <div className="container">
+        <p className="text-3xl font-bold text-black">{title}</p>
+      </div>
+    </div>
+  );
+}
